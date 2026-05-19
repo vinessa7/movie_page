2244d236ad8aae3883322d86ee1a2792e7efa01c
vinessa7 <voskanyaninessa8@gmail.com>
2026-05-19 14:47:06 +0400

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -1,24 +1,33 @@
 export default async function handler(req, res) {
-  const apiKey = process.env.apiKey;
+const apiKey = process.env.apiKey;
 
-  const endpoint = req.query.endpoint || "movie/popular";
+try {
+const { endpoint, ...query } = req.query;
 
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/${endpoint}`, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        "Content-Type": "application/json",
-      },
-    });
+```
+const params = new URLSearchParams(query);
 
-    ```
+const url = `https://api.themoviedb.org/3/${endpoint}?${params}`;
+
+const response = await fetch(url, {
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+    "Content-Type": "application/json",
+  },
+});
+
 const data = await response.json();
 
+if (!response.ok) {
+  return res.status(response.status).json(data);
+}
+
 res.status(200).json(data);
-```;
-  } catch (error) {
-    res.status(500).json({
-      error: "Failed to fetch TMDB data",
-    });
-  }
+```
+
+} catch (error) {
+res.status(500).json({
+error: error.message,
+});
+}
 }
